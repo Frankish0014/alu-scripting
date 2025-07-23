0ca1a7195bccc6eb88c3309e4aaaaab87f645384
Frankish0014 <f.ishimwe@alustudent.com>
2025-07-23 20:20:14 +0200

    Checking how many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import sys
 import requests
 
 def number_of_subscribers(subreddit):
@@ -8,17 +7,22 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers for a given subreddit.
     
     :param subreddit: The name of the subreddit (without the 'r/' prefix).
-    :return: The number of subscribers or None if the subreddit does not exist.
+    :return: The number of subscribers if the subreddit exists, otherwise 0.
     
+    This function queries the Reddit API and handles invalid subreddit names
+    by returning 0. It does not follow redirects.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'laptop:Mysubsapi/1.0.0 (by /u/NewsSuper8309)'}
-    response = requests.get(url, headers=headers)
-    sub = response['data']['subscribers']
     
-    if response.status_code !=200:
-        return sub
-    return 0
+    response = requests.get(url, headers=headers)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        return response.json()['data']['subscribers']
+    else:
+        return 0
 
-if __name__ == "_main__":
-    number_of_subscribers("python")
+if __name__ == "__main__":
+    print(number_of_subscribers("python"))
+
