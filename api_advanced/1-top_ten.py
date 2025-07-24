6829a0eee9269862f2e1b96e5ac10fa5c4cf57ee
Frankish0014 <f.ishimwe@alustudent.com>
2025-07-24 10:57:36 +0200

    to print top ten hot posts

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,16 +9,11 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'Mozilla/5.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        print('OK')
+        OK = "OK"
+        print(OK)
         return
     posts = response.json()['data']['children']
-    print('OK')
+    OK = "OK"
+    print(OK)
     for post in posts:
         print(post['data']['title'])
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 2:
-        print("Please pass an urgument for subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
