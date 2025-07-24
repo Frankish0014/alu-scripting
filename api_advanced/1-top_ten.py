ed7c3e06ea8cc5eda0f24c7dc6611054b038fe50
Frankish0014 <f.ishimwe@alustudent.com>
2025-07-24 09:21:12 +0200

    to print top ten hot posts

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,18 +2,17 @@
 """Top Ten File"""
 import requests
 
+
 def top_ten(subreddit):
     """
     Returns the titles of the top ten hot posts for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'laptop:Mysubsapi/1.0.0 (by /u/NewsSuper8309)'}
-    response = requests.get(url, headers=headers)
-    
+    headers = {'User-Agent': 'laptop:Mysubsapi/1.0.0 (by /u/NewsSuper8309)'
+               } # User-Agent
+    response = requests.get(url, headers=headers)   
     if response.status_code != 200:
         return None
-    
     data = response.json()
     posts = data.get('data', {}).get('children', [])
-    
     return [post['data']['title'] for post in posts]
