0f3ec42acf03b51a9a63d3bacd864fd3dcf19eb2
Frankish0014 <f.ishimwe@alustudent.com>
2025-07-24 08:42:14 +0200

    No of subs

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-
 """
-This module provides a function to retrieve the number of subscribers 
-for a given subreddit from Reddit's API.
-
-Function:
-- number_of_subscribers(subreddit): Returns the number of subscribers 
-  for the specified subreddit.
+function to query the 'Reddit API' and return the number of subscribers
 """
-
 import requests
+
 
 def number_of_subscribers(subreddit):
     """
     Returns the number of subscribers for a given subreddit.
-    
-    :param subreddit: The name of the subreddit (without the 'r/' prefix).
-    :return: The number of subscribers if the subreddit exists, otherwise 0.
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'laptop:Mysubsapi/1.0.0 (by /u/NewsSuper8309)'}
